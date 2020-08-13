81af2adde29d02cdd08815eb929c0be8b6514621
VHP-2994 <priyavenkat2994@gmail.com>
2020-08-13 13:08:20 +0530

    eureka client

--- src/main/java/com/spares/dealer/applicaiton/DealerApplication.java
+++ src/main/java/com/spares/dealer/applicaiton/DealerApplication.java
@@ -2,7 +2,9 @@ package com.spares.dealer.applicaiton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DealerApplication {
 
