2c5b15e73261ee93bf52d2c128a48962f4a9c8d6
RanganathanKP <ranganathankp@gmail.com>
2020-08-17 22:40:13 +0530

    swagger and rest

--- src/main/java/com/spares/dealer/repository/ProductRepository.java
+++ src/main/java/com/spares/dealer/repository/ProductRepository.java
@@ -3,12 +3,13 @@ package com.spares.dealer.repository;
 import com.spares.dealer.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer>{
 
     @Query(value = "SELECT * FROM test_spare.product where productcreationtime>(current_date()-1)", nativeQuery = true)
